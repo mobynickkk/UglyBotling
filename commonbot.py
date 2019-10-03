550494a94c12d67a160f50ef0017bf263ccba082
mobynickkk <44254290+mobynickkk@users.noreply.github.com>
2019-10-03 11:20:15 +0300

    пушится отлично

---
+++ commonbot.py
@@ -0,0 +1,7 @@
+class CommonBot:
+    """Класс для наследования в бота"""
+    def __init__(self):
+        """TODO: make objects of bots of vk and tg"""
+
+    def add_function(self, func: str):
+        """TODO: parse function from the string and add it to bot object"""
